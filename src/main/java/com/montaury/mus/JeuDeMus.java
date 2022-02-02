93e05e64dae6b79980355b91ca505e8be11abe8f
Xan MARIS <xmaris@iutbayonne.univ-pau.fr>
2022-02-02 14:59:49 +0100

    Ajout de la classe équipe & Modification du constructeur d'opposant (appelé dans jeuDeMus.java) & Modification des paramètres de méthodes dans JeuDeMus.

--- src/main/java/com/montaury/mus/JeuDeMus.java
+++ src/main/java/com/montaury/mus/JeuDeMus.java
@@ -5,15 +5,17 @@ import com.montaury.mus.console.AffichageEvenements;
 import com.montaury.mus.jeu.joueur.Joueur;
 import com.montaury.mus.jeu.Opposants;
 import java.util.Scanner;
+import com.montaury.mus.jeu.joueur.Equipe;
 
 public class JeuDeMus {
   public static void main(String[] args) {
     System.out.print("Entrez votre nom: ");
     var nomJoueur = new Scanner(System.in).next();
     var joueurHumain = Joueur.humain(nomJoueur);
-
+    var equipe1 = new Equipe (1, joueurHumain, Joueur.ordinateur());
+    var equipe2 = new Equipe (2, Joueur.ordinateur(), Joueur.ordinateur());
     var partie = new Partie(new AffichageEvenements(joueurHumain));
-    var resultat = partie.jouer(new Opposants(joueurHumain, Joueur.ordinateur()));
+    var resultat = partie.jouer(new Opposants(equipe1, equipe2));
 
     System.out.println("Le vainqueur de la partie est " + resultat.vainqueur().nom());
   }
--- src/main/java/com/montaury/mus/jeu/Opposants.java
+++ src/main/java/com/montaury/mus/jeu/Opposants.java
@@ -3,14 +3,21 @@ package com.montaury.mus.jeu;
 import com.montaury.mus.jeu.joueur.Joueur;
 import java.util.Iterator;
 import java.util.List;
+import com.montaury.mus.jeu.joueur.Equipe;
 
 public class Opposants {
   private Joueur joueurEsku;
   private Joueur joueurZaku;
+  private Joueur joueurNeutre1;
+  private Joueur joueurNeutre2;
 
-  public Opposants(Joueur joueurEsku, Joueur joueurZaku) {
-    this.joueurEsku = joueurEsku;
-    this.joueurZaku = joueurZaku;
+  public Opposants(Equipe equipe1,Equipe equipe2) {
+    this.joueurEsku = equipe1.getJoueur1();
+    this.joueurZaku = equipe2.getJoueur1();
+    this.joueurNeutre1 = equipe1.getJoueur2();
+    this.joueurNeutre2 = equipe2.getJoueur2();
+
+
   }
 
   public void tourner() {
