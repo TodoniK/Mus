3aa2ed4872e756e5dce6a0cef9b82baf3e84476e
Xan MARIS <xmaris@iutbayonne.univ-pau.fr>
2022-02-02 15:02:13 +0100

    MAJ : ajout du fichier equipe (échappé)

---
+++ src/main/java/com/montaury/mus/jeu/joueur/Equipe.java
@@ -0,0 +1,44 @@
+package com.montaury.mus.jeu.joueur;
+import java.util.List;
+import java.util.*;
+
+
+public class Equipe {
+
+    // Attriuts
+    private Joueur joueur1;
+    private Joueur joueur2;
+    private int num;
+
+    // Constructeur
+    public Equipe(int i, Joueur j1,Joueur j2)
+    {
+        this.joueur1 = j1;
+        this.joueur2 = j2;
+        this.num = i;
+    }
+
+    public Joueur getJoueur1() {
+        return joueur1;
+    }
+
+    public void setJoueur1(Joueur joueur1) {
+        this.joueur1 = joueur1;
+    }
+
+    public Joueur getJoueur2() {
+        return joueur2;
+    }
+
+    public void setJoueur2(Joueur joueur2) {
+        this.joueur2 = joueur2;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
